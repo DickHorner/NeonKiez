5a88ccc03b6ecb661fc985fe4b0619a2990dd25e
J Luetkens <megahoschi@googlemail.com>
2026-08-15 23:27:06 +0200

    feat: add mode-scoped hub palette

---
+++ hub_palette.ts
@@ -0,0 +1,37 @@
+const DEFAULT_ARCADE_PALETTE = hex`
+  000000
+  ffffff
+  ff2121
+  ff93c4
+  ff8135
+  fff609
+  249ca3
+  78dc52
+  003fad
+  87f2ff
+  8e2ec4
+  a4839f
+  5c406c
+  e5cdc4
+  91463d
+  000000
+`;
+
+const HUB_URBAN_PALETTE = hex`
+  000000
+  e6eef8
+  5c6278
+  aaa8bd
+  a09cca
+  f5aa57
+  beb8cd
+  38cbab
+  576cb6
+  b2acc1
+  69717b
+  836a62
+  c6bc9f
+  296360
+  39a077
+  7a77a4
+`;
