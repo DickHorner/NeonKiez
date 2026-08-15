7f745441c6d3db513e632bfe0f8943eac0cb1711
J Luetkens <megahoschi@googlemail.com>
2026-08-16 00:34:47 +0200

    feat: add generated hub tilemap adapter

---
+++ tilemap.g.ts
@@ -0,0 +1,105 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanPavement0036 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanPavement0066 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanRoad0441 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanRoad0433 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanRoad0460 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0329 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0360 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0365 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanPavement0035 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanPavement0037 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanRoad0461 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanRoad0463 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanRoad0442 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanRoad0469 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0328 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0330 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0359 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0361 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const rpgUrbanSavehouseFacade0366 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "TM_HUB_11":return tiles.createTilemap(hex`10000c00090f060606060606060606060606100a091107070707070713130707070707120a0901010101010101010101010101010a0901010101010101010101010101010a0901010101010101010101010101010a0901010101010101010101010101010a0902020202020202020202020202020a0b03030303030303030303030303030c0b03040304030403040304030403040c0b0303030d03030303030e030303030c0b03030303030303030303030303030c020202020202020303020202020202`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 2 2 2 2 2 2 . . 2 2 2 2 2 2 2
+`, [myTiles.transparency16,myTiles.rpgUrbanPavement0036,myTiles.rpgUrbanPavement0066,myTiles.rpgUrbanRoad0441,myTiles.rpgUrbanRoad0433,myTiles.rpgUrbanRoad0460,myTiles.rpgUrbanSavehouseFacade0329,myTiles.rpgUrbanSavehouseFacade0360,myTiles.rpgUrbanSavehouseFacade0365,myTiles.rpgUrbanPavement0035,myTiles.rpgUrbanPavement0037,myTiles.rpgUrbanRoad0461,myTiles.rpgUrbanRoad0463,myTiles.rpgUrbanRoad0442,myTiles.rpgUrbanRoad0469,myTiles.rpgUrbanSavehouseFacade0328,myTiles.rpgUrbanSavehouseFacade0330,myTiles.rpgUrbanSavehouseFacade0359,myTiles.rpgUrbanSavehouseFacade0361,myTiles.rpgUrbanSavehouseFacade0366], TileScale.Sixteen);
+            case "TM_HUB_21":return tiles.createTilemap(hex`10000c00020202020202020101020202020202020201010101010101010101010101010202010101010101010101010101010102020101010101010101010101010101020201010101010101010101010101010202010101010101010101010101010102020101010101010101010101010101020201010101010101010101010101010202030303030303030303030303030302020303030303030303030303030303020201010101010101010101010101010202020202020202020202020202020202`, img`
+2 2 2 2 2 2 2 . . 2 2 2 2 2 2 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 . . . . . . . . . . . . . . 2
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+`, [myTiles.transparency16,myTiles.rpgUrbanPavement0036,myTiles.rpgUrbanSavehouseFacade0365,myTiles.rpgUrbanRoad0441], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "rpgUrbanPavement0036":return rpgUrbanPavement0036;
+            case "rpgUrbanPavement0066":return rpgUrbanPavement0066;
+            case "rpgUrbanRoad0441":return rpgUrbanRoad0441;
+            case "rpgUrbanRoad0433":return rpgUrbanRoad0433;
+            case "rpgUrbanRoad0460":return rpgUrbanRoad0460;
+            case "rpgUrbanSavehouseFacade0329":return rpgUrbanSavehouseFacade0329;
+            case "rpgUrbanSavehouseFacade0360":return rpgUrbanSavehouseFacade0360;
+            case "rpgUrbanSavehouseFacade0365":return rpgUrbanSavehouseFacade0365;
+            case "rpgUrbanPavement0035":return rpgUrbanPavement0035;
+            case "rpgUrbanPavement0037":return rpgUrbanPavement0037;
+            case "rpgUrbanRoad0461":return rpgUrbanRoad0461;
+            case "rpgUrbanRoad0463":return rpgUrbanRoad0463;
+            case "rpgUrbanRoad0442":return rpgUrbanRoad0442;
+            case "rpgUrbanRoad0469":return rpgUrbanRoad0469;
+            case "rpgUrbanSavehouseFacade0328":return rpgUrbanSavehouseFacade0328;
+            case "rpgUrbanSavehouseFacade0330":return rpgUrbanSavehouseFacade0330;
+            case "rpgUrbanSavehouseFacade0359":return rpgUrbanSavehouseFacade0359;
+            case "rpgUrbanSavehouseFacade0361":return rpgUrbanSavehouseFacade0361;
+            case "rpgUrbanSavehouseFacade0366":return rpgUrbanSavehouseFacade0366;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
